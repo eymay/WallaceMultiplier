11fe369048779c1fcb985b8db5fac7f14ed532cc
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2023-04-01 17:46:33 +0300

    Parameterised full adder

---
+++ src/FA.v
@@ -0,0 +1,14 @@
+/* verilator lint_off WIDTH */
+module FA #(
+    parameter N = 32
+)(
+    input [N-1:0] A,
+    input [N-1:0] B,
+    input [N-1:0] Cin,
+    output reg [N-1:0] S,
+    output reg [N-1:0] Cout
+    );
+    
+assign {Cout, S} = A + B + Cin;
+
+endmodule
